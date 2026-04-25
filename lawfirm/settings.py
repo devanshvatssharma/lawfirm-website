3e19d951aa150fc167370a2a51954e3dafdb01ae
Devansh Sharma <devanshvatssharma@gmail.com>
2026-04-25 22:51:49 +0530

    updated settings for deployment

--- lawfirm/settings.py
+++ lawfirm/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bym6_7=12-+fde04o0sj75x57vjc3(q&nl2#v4ny%dmwd+7sl4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG') == 'True'
+
 
 ALLOWED_HOSTS = ["*"]
 
